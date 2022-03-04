d67f4369397447ea205f483f7380841638b703b4
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2022-03-04 12:44:52 +0100

    Display user location

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,9 +6,57 @@
 
 <script setup>
 import L from "leaflet";
-import { onMounted, reactive } from "vue";
+import { onMounted, ref, reactive } from "vue";
 
 let map = reactive();
+
+const coords = ref(null);
+const fetchCoords = ref(null);
+const geoMarker = ref(null);
+
+const getGeolocation = () => {
+  if (sessionStorage.getItem("coords")) {
+    coords.value = JSON.parse(sessionStorage.getItem("coords"));
+    plotGeolocation(coords.value);
+    return;
+  }
+
+  fetchCoords.value = true;
+
+  navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+};
+
+const setCoords = (pos) => {
+  fetchCoords.value = false;
+
+  const setSessionCoords = {
+    lat: pos.coords.latitude,
+    lng: pos.coords.longitude,
+  };
+
+  sessionStorage.setItem("coords", JSON.stringify(setSessionCoords));
+
+  coords.value = setSessionCoords;
+
+  plotGeolocation(coords.value);
+};
+
+const getLocError = (err) => {
+  console.log(err);
+};
+
+const plotGeolocation = (coords) => {
+  const customMarker = L.icon({
+    iconUrl: require("../assets/map-marker-red.svg"),
+    iconSize: [35, 35],
+  });
+
+  geoMarker.value = L.marker([coords.lat, coords.lng], {
+    icon: customMarker,
+  }).addTo(map);
+
+  map.setView([coords.lat, coords.lng], 10);
+};
 
 onMounted(() => {
   map = L.map("map").setView([51.505, -0.09], 13);
@@ -25,5 +73,7 @@ onMounted(() => {
       accessToken: process.env.VUE_APP_API_KEY,
     }
   ).addTo(map);
+
+  getGeolocation();
 });
 </script>
