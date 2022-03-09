8d1578d38bdc840011d85bba7f0c8109d8f18717
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2022-03-09 13:20:53 +0100

    Fix route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 const searchRoutes = require("./routes/searchRoutes");
 
 require("dotenv").config();
@@ -11,18 +10,6 @@ const app = express();
 
 app.use(cors());
 app.use("/api/search", searchRoutes);
-
-app.get("/api/search/:query", async (req, res) => {
-  try {
-    const query = req.params.query;
-    const { data } = await axios.get(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`
-    );
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(`${__dirname}/public`));
